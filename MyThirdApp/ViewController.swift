27e702cf8a6c952fd3a0c859bbfc98774e3dde00
vmolokov <vmolokov59@gmail.com>
2020-05-25 11:21:03 +0500

    Initial Commit

---
+++ MyThirdApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyThirdApp
+//
+//  Created by Виталик Молоков on 25.05.2020.
+//  Copyright © 2020 Виталик Молоков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
